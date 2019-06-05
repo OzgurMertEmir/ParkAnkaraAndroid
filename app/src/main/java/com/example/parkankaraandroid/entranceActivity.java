23b2b6d9831c5b73516d1e325bb8ba1c1000e10b
PC52 <cckilic2000@gmail.com>
2019-06-05 14:49:28 +0300

    EntranceActivity

---
+++ app/src/main/java/com/example/parkankaraandroid/entranceActivity.java
@@ -0,0 +1,31 @@
+package com.example.parkankaraandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class entranceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrance);
+    }
+
+    protected void seeTheMap(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intent);
+    }
+
+    protected void chooseLocation(View view){
+        Intent intent = new Intent(getApplicationContext(), Locations.class);
+        startActivity(intent);
+    }
+
+    protected void favoriteCarParks(View view){
+        Intent intent = new Intent(getApplicationContext(), FavoriteCarParks.class);
+        startActivity(intent);
+    }
+}
