acf0f42a9f6bcd3bcd12c0f7837b55d93e2458f2
OzgurMertEmir <gsmertemir@hotmail.com>
2019-06-11 01:35:24 +0300

    1:35 6/11/2019

--- app/src/main/java/com/example/parkankaraandroid/Locations.java
+++ app/src/main/java/com/example/parkankaraandroid/Locations.java
@@ -1,16 +1,7 @@
 package com.example.parkankaraandroid;
 
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
-import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -18,18 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import static android.content.ContentValues.TAG;
 
@@ -40,7 +20,7 @@ public class Locations extends AppCompatActivity {
     ArrayList<String> cpName;
     ArrayList<String> cpAddress;
     ListView listView;
-    LocationsPostClass adapter;
+    static LocationsPostClass adapter;
     ArrayList<String> latitude;
     ArrayList<String> longitude;
 
