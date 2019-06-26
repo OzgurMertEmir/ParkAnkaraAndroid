0b2a95ef2a0fb29e80934ef82d9395585c5bca42
GoktugCagiran <cagirang@gmail.com>
2019-06-26 20:12:12 +0300

    LocationsPostClass commit 26/06/2019 20:11 ToggleButton final fix

--- app/src/main/java/com/example/parkankaraandroid/LocationsPostClass.java
+++ app/src/main/java/com/example/parkankaraandroid/LocationsPostClass.java
@@ -24,9 +24,10 @@ public class LocationsPostClass extends ArrayAdapter<String> {
     private final ArrayList<String> cpAddress;
     private final ArrayList<String> cpCondition;
     private final Activity context;
+    private final ArrayList<Boolean> favourited;
     static SharedPreferences sharedPreferences;
     Set<String> favorites;
-    ToggleButton toggleButton;
+    //ToggleButton toggleButton;
 
     /**
      *
@@ -41,8 +42,16 @@ public class LocationsPostClass extends ArrayAdapter<String> {
         this.cpAddress = cpAddress;
         this.cpCondition = cpCondition;
         this.context = context;
+        favourited = new ArrayList<>();
         sharedPreferences = context.getSharedPreferences("com.example.parkankara", Context.MODE_PRIVATE);
         favorites = new HashSet<>( sharedPreferences.getStringSet("CarParkName", new HashSet<String>()) );
+        for(String name : cpName){
+            if(favorites.contains(name)){
+                favourited.add(true);
+            }else{
+                favourited.add(false);
+            }
+        }
     }
 
     /**
@@ -61,33 +70,35 @@ public class LocationsPostClass extends ArrayAdapter<String> {
         TextView cpNameText = carParkInfo.findViewById(R.id.cpName);
         TextView locationText = carParkInfo.findViewById(R.id.address);
         TextView conditionText = carParkInfo.findViewById(R.id.condition);
-        toggleButton = carParkInfo.findViewById(R.id.favoriteButton);
-
-
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                if (!favorites.contains(cpName.get(position))) {
-                    buttonView.setChecked(true);
-                    favorites.add(cpName.get(position));
-                    editor.putStringSet("CarParkName", favorites).apply();
-                    editor.commit();
-                    Toast.makeText(context, cpName.get(position) + " Favori otoparklara eklendı!", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    favorites.remove(cpName.get(position));
-                    buttonView.setChecked(false);
-                    editor.putStringSet("CarParkName", favorites).apply();
-                    editor.commit();
-                    Toast.makeText(context, cpName.get(position) + " Favori otoparklardan çıkarıldı!", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        final ToggleButton toggleButton = carParkInfo.findViewById(R.id.favoriteButton);
 
         //set method  of components
         cpNameText.setText(cpName.get(position));
         locationText.setText(cpAddress.get(position));
+        toggleButton.setChecked(favourited.get(position));
+
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            @Override
+            public void onClick(View v) {
+                if(favourited.get(position)){
+                    favorites.remove(cpName.get(position));
+                    toggleButton.setChecked(false);
+                    favourited.set(position, false);
+                    editor.putStringSet("CarParkName", favorites).apply();
+                    editor.commit();
+                    Toast.makeText(context, cpName.get(position) + " Favori otoparklardan çıkarıldı!", Toast.LENGTH_LONG).show();
+                }else{
+                    favorites.add(cpName.get(position));
+                    toggleButton.setChecked(true);
+                    favourited.set(position, true);
+                    editor.putStringSet("CarParkName", favorites).apply();
+                    editor.commit();
+                    Toast.makeText(context, cpName.get(position) + " Favori otoparklara eklendı!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         //Checks if the car park is full and then writes the appropriate text to TextView
         if(Integer.parseInt( cpCondition.get(position) ) <= 0)
@@ -97,7 +108,6 @@ public class LocationsPostClass extends ArrayAdapter<String> {
         else {
             conditionText.setText(cpCondition.get(position) + " tane boş yer mevcut");
         }
-
         return carParkInfo;
     }
 
